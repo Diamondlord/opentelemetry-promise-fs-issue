47cfa8da92c0269dad7a768c25dbe8cbfac096f3
Vladimir Almazov <vladimir.almazov@gmail.com>
2023-02-05 19:34:42 +0200

    feat: add writeFile from fs/promises that causes issue

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { writeFile } from 'fs/promises';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+  await writeFile('./test.txt', 'I dont work with opentelemetry');
 }
 bootstrap();
